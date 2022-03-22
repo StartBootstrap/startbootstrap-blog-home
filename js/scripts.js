fb2b4db296a3190a54ad7821caa26fe6efcb8535
David Miller <hello@davidmiller.io>
2022-03-22 16:34:10 -0400

    dependencies update

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
-* Start Bootstrap - Blog Home v5.0.7 (https://startbootstrap.com/template/blog-home)
-* Copyright 2013-2021 Start Bootstrap
+* Start Bootstrap - Blog Home v5.0.8 (https://startbootstrap.com/template/blog-home)
+* Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-blog-home/blob/master/LICENSE)
 */
 // This file is intentionally blank
